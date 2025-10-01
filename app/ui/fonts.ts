37dd0e194639b67bfb92a86bacc943b17c8feceb
anitacheung83 <anitacheung83@hotmail.com>
2025-10-01 15:15:44 -0400

    Fix: google font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { DM_Serif_Display, Quicksand } from "next/font/google";
 
-export const dmSerifDisplay = DM_Serif_Display({ weight: "400" })
+export const dmSerifDisplay = DM_Serif_Display({ weight: "400" , display: "swap"})
 
-export const quicksand = Quicksand({ weight: "300" })
+export const quicksand = Quicksand({ weight: "300", display: "swap" })
